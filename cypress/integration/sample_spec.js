36ea1e99a7f1a6a065eb5d66c06aad73ca87b6c7
Brian Olore <brian@olore.net>
2020-04-02 14:46:04 -0400

    chore: github workflow to run cypress on push

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -5,18 +5,32 @@ const startButton = '.is-hidden-desktop > .is-primary';
 const nameInput = '[data-testid=text-input-firstName]';
 const joinButton = '[data-testid=add-player] > form > .field > :nth-child(2) > .button';
 
+let url = 'http://localhost:3000';
+
+if (process.env.GITHUB_ACTION) {
+  url = 'http://localhost:4000';
+}
+
 describe('The Home Page', function() {
   it('successfully loads', function() {
-    cy.visit('http://localhost:3000');
+    cy.log("hey it's brian");
+    cy.log(process.env);
+    cy.log(process.env.GITHUB_ACTION);
+    cy.log("bye it's brian");
+
+    cy.visit(url);
   });
 
   it('allows starting a new game', function() {
-    cy.visit('http://localhost:3000');
+    cy.visit(url);
     cy.get(startButton).click();
     cy.get(nameInput).type('Bobby');
     cy.get(joinButton).click();
 
-    cy.get('.notification').should('contain', 'You look lonely');
+    cy.screenshot('my-image');
+
+    // default 4s not enough for github action? //10s either ??
+    cy.get('.notification', { timeout: 30000 }).should('contain', 'You look lonely');
     expect(cy.contains('2 Points')).to.exist;
   });
 });
